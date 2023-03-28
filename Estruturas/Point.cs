220f83508b008fc872729d3e781a010123ca97bd
Brizamar Caxias <brizamarcaxias@gmail.com>
2023-03-28 20:46:22 -0300

    chore: Example struct of Point.

---
+++ Estruturas/Point.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estruturas{
+    struct Point{
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "("+X+","+Y+")";
+        }
+
+    }
+}
---
+++ Estruturas/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Estruturas;
+
+Console.WriteLine("Hello, World!");
+Point p;
+p.X=10;
+p.Y=50;
+Console.WriteLine(p.ToString());
